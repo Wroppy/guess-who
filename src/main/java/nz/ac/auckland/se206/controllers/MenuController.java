a10b81ca6f03c7620742c06e708c545d2aeb1d47
Ken <d3056367993@gmail.com>
2024-09-13 15:31:40 +1200

    Implemented the function when start is clicked, scene changes to crime.

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -1,3 +1,23 @@
 package nz.ac.auckland.se206.controllers;
 
-public class MenuController {}
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.SceneManager.SceneType;
+
+public class MenuController {
+
+  @FXML private Button btnStart;
+
+  /** Initializes the menu view. */
+  @FXML
+  public void initialize() {
+    // Any required initialization code can be placed here
+  }
+
+  /** Handles the start button click event. */
+  @FXML
+  public void onStartButtonClick() {
+    App.changeScene(SceneType.CRIME);
+  }
+}
