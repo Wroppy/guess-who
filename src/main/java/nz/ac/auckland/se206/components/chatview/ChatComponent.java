76b019593ad96080b98d0c9bd930d0485b5ff6da
Weyman Wong <wxwong806@gmail.com>
2024-09-15 01:21:09 +1200

    Added loading component to the send message button

--- src/main/java/nz/ac/auckland/se206/components/chatview/ChatComponent.java
+++ src/main/java/nz/ac/auckland/se206/components/chatview/ChatComponent.java
@@ -3,14 +3,27 @@ package nz.ac.auckland.se206.components.chatview;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import nz.ac.auckland.se206.App;
 
 public class ChatComponent extends VBox {
+  private boolean loading;
+
+  @FXML private Pane sendMessageButton;
+
+  @FXML private TextField textInput;
+  @FXML private TextArea chatBox;
+
+  private LoaderComponent loaderComponent;
 
   public ChatComponent() {
+    this.loading = false;
+
     try {
       FXMLLoader loader = App.loadFxmlLoader("chat");
 
@@ -19,10 +32,26 @@ public class ChatComponent extends VBox {
 
       loader.load();
 
+      setupButton();
+
       // TODO: Set up the GPT model for the suspect
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private void setupButton() {
+    loaderComponent = new LoaderComponent();
+    loaderComponent.setPrefWidth(
+        sendMessageButton.getPrefWidth() - this.getPadding().getBottom() * 2);
+    loaderComponent.setStyle(
+        "-fx-border-color: black; -fx-border-width: 1px; -fx-border-style: solid;");
+    sendMessageButton.getChildren().add(loaderComponent);
+
+    // Adds it to the center of the layout
+    // loaderComponent.setLayoutX((sendMessageButton.getWidth() - loaderComponent.getWidth()) / 2);
+    // loaderComponent.setLayoutY((sendMessageButton.getHeight() - loaderComponent.getHeight()) /
+    // 2);
   }
 
   /**
@@ -40,4 +69,15 @@ public class ChatComponent extends VBox {
    */
   @FXML
   private void onKeyPressed(KeyEvent e) {}
+
+  private void setLoading(boolean loading) {
+    this.loading = loading;
+
+    // Shows or hides loading for user experience
+    if (loading) {
+
+    } else {
+
+    }
+  }
 }
