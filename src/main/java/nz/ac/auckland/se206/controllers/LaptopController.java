7777ba7eebb7ffe7b13d014a64c8a1799ab9e739
Arnav <arnavbhatiani1904@gmail.com>
2024-09-16 14:54:27 +1200

    Added an email that lets the user know what the access code is to unlock the access code logs.

--- src/main/java/nz/ac/auckland/se206/controllers/LaptopController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LaptopController.java
@@ -13,23 +13,34 @@ import javafx.stage.Stage;
 public class LaptopController {
   @FXML private Rectangle performanceEmail; // pane of the perfomance email
   @FXML private Rectangle firingEmail; // pane of the firing email
+  @FXML private Rectangle accessEmail;
   @FXML private Pane performanceReport; // pane of the promotion email
   @FXML private Pane firedEmail; // pane of the email 
+  @FXML private Pane accessEmailPane;
 
   public void initialize() throws IOException {
     // // set the visibility of the panes to false
     performanceReport.setVisible(false);
     firedEmail.setVisible(false);
+    accessEmailPane.setVisible(false);
   }
 
   public void handleRectangleClick(MouseEvent event) {
     Rectangle clickedRectangle = (Rectangle) event.getSource();
+
     if(clickedRectangle == performanceEmail) {
       performanceReport.setVisible(true);
+      accessEmailPane.setVisible(false);
       firedEmail.setVisible(false);
+
     } else if(clickedRectangle == firingEmail) {
       firedEmail.setVisible(true);
+      accessEmailPane.setVisible(false);
       performanceReport.setVisible(false);
+
+    } else if(clickedRectangle == accessEmail) {
+      accessEmailPane.setVisible(true);
+      performanceReport.setVisible(false);  
     }
   }
   @FXML
