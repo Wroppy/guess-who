9e97e0bd9d3d70423fb21b20100695936d338267
Ken <d3056367993@gmail.com>
2024-09-17 19:26:26 +1200

    In menu controller, when start button is clicked, the timer is initialised.

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -3,11 +3,14 @@ package nz.ac.auckland.se206.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import nz.ac.auckland.se206.App;
+import nz.ac.auckland.se206.GameTimer;
 import nz.ac.auckland.se206.SceneManager.SceneType;
 
 public class MenuController {
 
   @FXML private Button btnStart;
+
+  public GameTimer gameTimer;
 
   /** Initializes the menu view. */
   @FXML
@@ -18,6 +21,9 @@ public class MenuController {
   /** Handles the start button click event. */
   @FXML
   public void onStartButtonClick() {
+    gameTimer =
+        new GameTimer(RoomController.getGameHeader().getTimerLabel(), RoomController.getContext());
+    gameTimer.start();
     App.changeScene(SceneType.CRIME);
   }
 }
