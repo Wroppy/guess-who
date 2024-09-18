464f84682980a80e6552f3991ae88cbf5e099c0a
Arnav <arnavbhatiani1904@gmail.com>
2024-09-18 14:24:33 +1200

    Added code to implement functionality of a submit button, so that they cannot click until they have written in the text are and clicked on a suspect.

--- src/main/java/nz/ac/auckland/se206/controllers/GuessingController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GuessingController.java
@@ -2,10 +2,14 @@ package nz.ac.auckland.se206.controllers;
 
 import java.io.IOException;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.SceneType;
 
@@ -14,10 +18,25 @@ public class GuessingController {
   @FXML private Rectangle bob;
   @FXML private Rectangle vicePresident;
   @FXML private Rectangle third;
-  
-
+  @FXML private Button submitBtn;
   private String explanation;
   private static boolean correctChoice;
+  private boolean isClicked = false;
+  
+
+  public void initialize() {
+    // Add a listener to check if TextArea has text input
+
+  Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+      if (explaintxt.getText() == null || explaintxt.getText().trim().isEmpty() || !isClicked) {
+        submitBtn.setDisable(true);
+      } else {
+        submitBtn.setDisable(false);
+      }
+    }));
+    timeline.setCycleCount(Timeline.INDEFINITE);
+    timeline.play();
+  }
 
   public void explanationScene(MouseEvent event) throws IOException {
     explanation = explaintxt.getText().trim();
@@ -25,6 +44,7 @@ public class GuessingController {
   }
 
   public void choiceCriminal(MouseEvent event) {
+    isClicked = true;
     Rectangle clickedRectangle = (Rectangle) event.getSource();
     if (clickedRectangle == vicePresident) {
       correctChoice = true;
