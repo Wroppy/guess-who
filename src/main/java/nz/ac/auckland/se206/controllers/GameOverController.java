0afd7f95ab2af6a5a79b846abdfeccb615ad807b
Weyman Wong <wxwong806@gmail.com>
2024-09-19 23:25:50 +1200

    Changed incorrect guess scene to be less like a void

--- src/main/java/nz/ac/auckland/se206/controllers/GameOverController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GameOverController.java
@@ -1,33 +1,47 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class GameOverController {
   @FXML private Label result;
   @FXML private TextArea feedbacktxt;
   @FXML private Text feedback;
+  @FXML private Button playAgain;
 
   private static Label resultLabel;
   private static Text feedbackLabel;
   private static TextArea feedbackTextArea;
+  private static Button playAgainButton;
 
   public void initialize() {
     GameOverController.resultLabel = result;
     GameOverController.feedbackLabel = feedback;
     GameOverController.feedbackTextArea = feedbacktxt;
+    GameOverController.playAgainButton = playAgain;
   }
 
   public static void showResult() {
     if (GuessingController.getCorrectChoice()) {
       GameOverController.resultLabel.setText("Correct Choice!");
+      GameOverController.resultLabel.setLayoutY(106);
+
+      playAgainButton.setLayoutX(335);
+      playAgainButton.setLayoutY(507);
+
     } else {
       GameOverController.resultLabel.setText("Incorrect Choice!");
+      GameOverController.resultLabel.setFont(Font.font("System", 28));
+      GameOverController.resultLabel.setLayoutY(250);
       GameOverController.feedbackTextArea.setVisible(false);
       GameOverController.feedbackLabel.setVisible(false);
-    }
 
+      playAgainButton.setLayoutX(335);
+      playAgainButton.setLayoutY(450);
+    }
   }
 }
