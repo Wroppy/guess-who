2f29563aae7060f5093426ba9846ad8655630d51
Arnav <arnavbhatiani1904@gmail.com>
2024-10-06 20:21:58 +1300

    Added placeholder emails for more immersion as discussed.

--- src/main/java/nz/ac/auckland/se206/controllers/LaptopController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LaptopController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
@@ -22,6 +23,10 @@ public class LaptopController {
 
   @FXML private Rectangle performanceEmail; // pane of the perfomance email
   @FXML private Rectangle firingEmail; // pane of the firing email
+  @FXML private Rectangle fillerEmail_1;
+  @FXML private Rectangle fillerEmail_2;
+  @FXML private Pane fillerEmailPane1;
+  @FXML private Pane fillerEmailPane2;  
   @FXML private Pane performanceReport; // pane of the promotion email
   @FXML private Pane firedEmail; // pane of the email
 
@@ -29,6 +34,8 @@ public class LaptopController {
     // // set the visibility of the panes to false
     performanceReport.setVisible(false);
     firedEmail.setVisible(false);
+    fillerEmailPane1.setVisible(false);
+    fillerEmailPane2.setVisible(false);
   }
 
   public void handleRectangleClick(MouseEvent event) {
@@ -40,10 +47,24 @@ public class LaptopController {
     if (clickedRectangle == performanceEmail) {
       performanceReport.setVisible(true);
       firedEmail.setVisible(false);
+      fillerEmailPane1.setVisible(false);
+      fillerEmailPane2.setVisible(false);
 
     } else if (clickedRectangle == firingEmail) {
       firedEmail.setVisible(true);
       performanceReport.setVisible(false);
+      fillerEmailPane1.setVisible(false);
+      fillerEmailPane2.setVisible(false);
+    } else if (clickedRectangle == fillerEmail_1) {
+      fillerEmailPane1.setVisible(true);
+      performanceReport.setVisible(false);
+      firedEmail.setVisible(false);
+      fillerEmailPane2.setVisible(false);
+    } else if (clickedRectangle == fillerEmail_2) {
+      fillerEmailPane2.setVisible(true);
+      performanceReport.setVisible(false);
+      firedEmail.setVisible(false);
+      fillerEmailPane1.setVisible(false);
     }
   }
 
