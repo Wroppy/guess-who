aa891d1a7c12e76ebd18550b679fa234022759d0
Weyman Wong <wxwong806@gmail.com>
2024-10-11 00:34:32 +1300

    Added testing label for font checking on mac os

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -26,6 +26,10 @@ public class MenuController implements Restartable {
     // Any required initialization code can be placed here
     SoundManager.playSound("Welcome.mp3");
 
+
+    // Initialize the font
+    App.getFont("PermanentMarker", 22);
+
     this.styleScene();
   }
 
