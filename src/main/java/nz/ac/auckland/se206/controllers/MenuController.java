b3bb61254004cec46ee6f30c0f65b374aca37b97
Weyman Wong <wxwong806@gmail.com>
2024-10-10 13:14:01 +1300

    Removed labels and changed them to images in the background instead

--- src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/MenuController.java
@@ -3,8 +3,9 @@ package nz.ac.auckland.se206.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameTimer;
 import nz.ac.auckland.se206.SceneManager.SceneType;
@@ -15,8 +16,6 @@ public class MenuController implements Restartable {
 
   @FXML private Pane menuPane;
   @FXML private Button investigateButton;
-  @FXML private Label guessLabel;
-  @FXML private Label suspectLabel;
   @FXML private Label briefLabel;
 
   private GuessingController guessingController;
@@ -27,14 +26,19 @@ public class MenuController implements Restartable {
     // Any required initialization code can be placed here
     SoundManager.playSound("Welcome.mp3");
 
-    App.getMarkerFont(18);
-    App.getFont("AfacadFlux", 22);
     this.styleScene();
   }
 
   private void styleScene() {
     String style = App.getCssUrl("menu-scene");
     menuPane.getStylesheets().add(style);
+
+    Image image =
+        new Image(App.class.getResource("/images/start-investigation-text.png").toExternalForm());
+    ImageView imageView = new ImageView(image);
+    imageView.setFitWidth(170);
+    imageView.preserveRatioProperty().setValue(true);
+    investigateButton.setGraphic(imageView);
   }
 
   /** Handles the start button click event. */
